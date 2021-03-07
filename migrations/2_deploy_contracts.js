45bbf1ce27358e2f5a3d4d7360fdf35ab1f35187
Reason <Reason@Lee.com>
2021-03-07 10:24:08 +0800

    total supply 200 million

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,10 +2,10 @@ var Art = artifacts.require('./Art.sol');
 var Token = artifacts.require('./Token.sol')
 
 module.exports = function(deployer) {
-
-  deployer.deploy(Token, "Hello NFC", "HNC", "1000000000000000000000000").then(function() {
+  deployer.deploy(Token, "Hello NFC", "HNC", "20000000000000000000000000").then(function() {
     // return deployer.deploy(Art, Token.address);
     return deployer.deploy(Art, Token.address).then(function(instance){
+
       instance.registe("0x1111111111111111111111111111111111111111", 1, 10000); 
       instance.registe("0x2222222222222222222222222222222222222222", 2, 10000);
       instance.registe("0x3333333333333333333333333333333333333333", 3, 10000); 
